87b318402180f8fb163a64ac54faf80cf307fac8
Chris P laptop <up946626@myport.ac.uk>
2023-06-25 20:21:20 +0100

    theming

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <button @click="toggleTheme">Toggle Theme</button>
     <NavigationBar></NavigationBar>
     <router-view></router-view>
   </div>
@@ -13,7 +14,25 @@ export default {
   components: {
     NavigationBar
   },
+  mounted() {
+    const root = document.documentElement;
+    root.classList.add('light-theme');
+  },
+  methods: {
+    toggleTheme() {
+      const root = document.documentElement;
+      if (root.classList.contains('dark-theme')) {
+        root.classList.remove('dark-theme');
+        root.classList.add('light-theme');
+      } else {
+        root.classList.remove('light-theme');
+        root.classList.add('dark-theme');
+      }
+    },
+},
 };
+
+
 </script>
 
 <style scoped>
